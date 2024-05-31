fd3a847648637f2c768c33fadb74514630ab89c7
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-05-31 11:18:02 +0100

    Ensure that default constructor works

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,15 @@
 using MistyClosures, Test
 
 using Core: OpaqueClosure
-using Core.Compiler: IRCode
 
 @testset "MistyClosures.jl" begin
     ir = Base.code_ircode_by_type(Tuple{typeof(sin), Float64})[1][1]
+
+    # Recommended constructor.
     mc = MistyClosure(ir; do_compile=true)
     @test @inferred(mc(5.0)) == sin(5.0)
+
+    # Default constructor.
+    mc_default = MistyClosure(OpaqueClosure(ir; do_compile=true), ir)
+    @test @inferred(mc_default(5.0) == sin(5.0))
 end
