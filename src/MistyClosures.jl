ee522725df94c46c987c7e3e9a0ccf8f9c8fa6a6
Will Tebbutt <wct23@cam.ac.uk>
2024-05-31 12:26:13 +0200

    First Attempt (#3)
    * Basics
    
    * Add inference test
    
    * Only run on 1.10
    
    * Add inner constructor
    
    * Enable default constructor
    
    * Ensure that default constructor works
    
    * Add small example in readme
    
    * Fix typo
    
    * Set to version 1
    
    * Tweak Project.toml

--- src/MistyClosures.jl
+++ src/MistyClosures.jl
@@ -1,5 +1,17 @@
 module MistyClosures
 
-# Write your package code here.
+using Core: OpaqueClosure
+using Core.Compiler: IRCode
+
+struct MistyClosure{Toc<:OpaqueClosure}
+    oc::Toc
+    ir::IRCode
+end
+
+MistyClosure(ir::IRCode; kwargs...) =  MistyClosure(OpaqueClosure(ir; kwargs...), ir)
+
+(mc::MistyClosure)(x::Vararg{Any, N}) where {N} = mc.oc(x...)
+
+export MistyClosure
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
-using MistyClosures
-using Test
+using MistyClosures, Test
+
+using Core: OpaqueClosure
 
 @testset "MistyClosures.jl" begin
-    # Write your tests here.
+    ir = Base.code_ircode_by_type(Tuple{typeof(sin), Float64})[1][1]
+
+    # Recommended constructor.
+    mc = MistyClosure(ir; do_compile=true)
+    @test @inferred(mc(5.0)) == sin(5.0)
+
+    # Default constructor.
+    mc_default = MistyClosure(OpaqueClosure(ir; do_compile=true), ir)
+    @test @inferred(mc_default(5.0) == sin(5.0))
 end
