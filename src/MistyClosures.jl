c41c02c91ad1a2d313f316c2a5c26ea4d22fcb93
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-08-21 17:47:22 +0100

    Implement better show

--- src/MistyClosures.jl
+++ src/MistyClosures.jl
@@ -3,6 +3,8 @@ module MistyClosures
 using Core: OpaqueClosure
 using Core.Compiler: IRCode
 
+import Base: show
+
 struct MistyClosure{Toc<:OpaqueClosure}
     oc::Toc
     ir::IRCode
@@ -15,6 +17,16 @@ MistyClosure(ir::IRCode; kwargs...) =  MistyClosure(OpaqueClosure(ir; kwargs...)
 # You can't deepcopy an `IRCode` because it contains a module.
 function Base.deepcopy_internal(x::T, dict::IdDict) where {T<:MistyClosure}
     return T(Base.deepcopy_internal(x.oc, dict), x.ir)
+end
+
+# Don't print out the IRCode object, because this tends to pollute the REPL. Just make it
+# clear that this is a MistyClosure, which contains an OpaqueClosure.
+show(io::IO, mime::MIME"text/plain", mc::MistyClosure) = _show_misty_closure(io, mime, mc)
+show(io::IO, mc::MistyClosure) = _show_misty_closure(io, MIME"text/plain"(), mc)
+
+function _show_misty_closure(io::IO, mime::MIME"text/plain", mc::MistyClosure)
+    print(io, "MistyClosure ")
+    show(io, mime, mc.oc)
 end
 
 export MistyClosure
